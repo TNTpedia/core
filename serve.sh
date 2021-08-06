0d98bb8f158c339348b05a099213179a4159aa99
kocotian <kocotian@kocotian.pl>
2021-08-06 21:19:08 +0000

    Using stac script

--- serve.sh
+++ serve.sh
@@ -12,9 +12,4 @@ echo -n "Server: nginx/stac\r\n"
 echo
 
 stat "in/$ARTICLE.stac" > /dev/null 2>&1 || ARTICLE="Specjalna:404"
-echo > "meta/$ARTICLE.c"
-./compile -t templates/basic.stac -o "meta/$ARTICLE.c" in/$ARTICLE.stac || ARTICLE="Specjalna:500"
-echo > "meta/$ARTICLE.bin"
-gcc -o meta/$ARTICLE.bin -I. meta/$ARTICLE.c util.o assemble.o str.o libstac/libstac.o 2>&1 || ARTICLE="Specjalna:500"
-meta/$ARTICLE.bin 2>&1 || ARTICLE="Specjalna:500"
-# cat "$DOCUMENT_ROOT$DOCUMENT_URI.html"
+./stac -t templates/basic.stac in/$ARTICLE.stac
